06e3fb155b7a1dba08816bd638559bb7b5cecd3b
Bukunmi <bkakinyemi21@gmail.com>
2025-04-03 15:44:40 +0100

    changed access token minutes

--- app/auth.py
+++ app/auth.py
@@ -11,7 +11,7 @@ from .db import get_db
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 360
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
